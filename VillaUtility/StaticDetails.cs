42f3817b1e8b3bd531f0b99eac6b44ad50eadc61
albraa abdalla <albraaengineer@gmail.com>
2023-06-11 17:05:27 +0200

    disable nullable

---
+++ VillaUtility/StaticDetails.cs
@@ -0,0 +1,16 @@
+﻿namespace VillaUtility
+{
+    public static class StaticDetails
+    {
+        public enum ApiType
+        {
+            GET,
+            POST,
+            PUT,
+            DELETE
+        }
+
+        public static string SessinToken = "JWTToken";
+
+    }
+}
