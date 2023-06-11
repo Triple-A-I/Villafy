b5988ce215ef50fe01eb99b825222b090f0a5d24
albraa abdalla <albraaengineer@gmail.com>
2023-06-11 17:08:42 +0200

    add villanumber dtos

---
+++ VillaWeb/Models/Dto/VillaNumber/VillaNumberDto.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+using VillaWeb.Models.Dto.Villa;
+
+namespace VillaWeb.Models.Dto.VillaNumber
+{
+    public class VillaNumberDto
+    {
+        public int VillaNo { get; set; }
+        public string SpecialDetails { get; set; }
+        [Required]
+
+        public int VillaId { get; set; }
+        public VillaDto Villa { get; set; }
+
+        public DateTime CreatedDate { get; set; }
+        public DateTime UpdatedDate { get; set; }
+    }
+}
