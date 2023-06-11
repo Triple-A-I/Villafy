78a55adbe8514010c62662198ba911ef3e8b7f36
albraa abdalla <albraaengineer@gmail.com>
2023-06-11 17:08:25 +0200

    add auth dtos

---
+++ VillaWeb/Models/Dto/Auth/LoginResponseDto.cs
@@ -0,0 +1,8 @@
+﻿namespace VillaWeb.Models.Dto.Auth
+{
+    public class LoginResponseDto
+    {
+        public LocalUser LocalUser { get; set; }
+        public string Token { get; set; }
+    }
+}
---
+++ VillaWeb/Models/Dto/Auth/RegistrationRequestDto.cs
@@ -0,0 +1,11 @@
+﻿namespace VillaWeb.Models.Dto.Auth
+{
+    public class RegistrationRequestDto
+    {
+        public string Name { get; set; }
+        public string UserName { get; set; }
+        public string Password { get; set; }
+        public string Role { get; set; }
+
+    }
+}
